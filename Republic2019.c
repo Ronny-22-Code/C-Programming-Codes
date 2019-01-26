b5e8e691329f3bab0c84579459be45889e069c2c
Rohan Sethi <mailtorohansethi@gmail.com>
2019-01-26 13:43:34 +0530

    A C source code file  for Republic Day of India .

---
+++ Republic2019.c
@@ -0,0 +1,26 @@
+//This code prints Happy Republic Day 70 times !!!!.
+
+#include<stdio.h>
+
+void main()
+
+{
+	
+	int i,j;
+	
+	printf("-------------------------------------------------------------------------");
+	
+	
+	for(i=0;i<70;i++)
+	
+	{
+		printf("Happy Republic Day India !!!!");
+		printf("\n");
+	}
+	
+	
+	printf("---------------------------------------------------------------------------");
+
+	
+}
+
